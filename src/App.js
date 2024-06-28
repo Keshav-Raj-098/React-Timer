d5bef9810fc92817f018dc98237b91758c474391
Keshav-Raj-098 <keshavraj09898@gmail.com>
2024-06-29 01:09:58 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Timer from "./components/timer.js";
+
+function App() {
+  return (
+    <>
+<Timer deadlinedate={4} month={9} year={2024} hour={0} minutes={0}/>
+
+     
+    </>
+  );
+}
+
+export default App;
